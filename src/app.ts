3230cff3fc49ca12df8822c03aa320cbf9e97f5e
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-27 15:34:48 +0200

    Fixing differences between actual code and screencast

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,3 @@
-type Engineer = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  books: string[];
-  id: number;
-  friends: string[];
-};
-
 const user = {
   firstName: "John",
   lastName: "Doe",
@@ -16,7 +7,16 @@ const user = {
   friends: ["Jane Smith", "Michael Johnson"],
 };
 
-function mergeProfil(
+function displayBooks(books: string[]): void {
+  books.forEach((book) => console.log(book));
+}
+
+function getFriends(firstName: string, friends: string[]): string {
+  const sentence = `Les amis de ${firstName} sont ${friends.join(", ")}`;
+  return sentence;
+}
+
+function handleProfil(
   firstName: string,
   lastName: string,
   age: number,
@@ -25,30 +25,21 @@ function mergeProfil(
   return [firstName, lastName, String(age), String(id)];
 }
 
-function displayBooks(books: string[]): void {
-  books.forEach((book) => console.log(book));
-}
-
-function getFriends(friends: string[]): string {
-  const sentence = `Les amis sont ${friends.join(", ")}.`;
-  return sentence;
-}
-
 function main(): void {
-  console.log("==========")
-  console.log("La fonction displayBooks")
-  displayBooks(user.books)
-  console.log("==========")
+  console.log("=======");
+  console.log("La fonction displayBooks");
+  displayBooks(user.books);
+  console.log("=======");
 
-  console.log("==========")
-  console.log("La fonction getFriends")
-  console.log(getFriends(user.friends))
-  console.log("==========")
+  console.log("=======");
+  console.log("La fonction getFriends");
+  console.log(getFriends(user.firstName, user.friends));
+  console.log("=======");
 
-  console.log("==========")
-  console.log("La fonction mergeProfile")
-  console.log(mergeProfil(user.firstName, user.lastName, user.age, user.id))
-  console.log("==========")
+  console.log("=======");
+  console.log("La fonction handleProfil");
+  console.log(handleProfil(user.firstName, user.lastName, user.age, user.id));
+  console.log("=======");
 }
 
 main();

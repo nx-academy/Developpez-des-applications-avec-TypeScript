b2d4a65adf5943761f3f17142504bdafd8fd9a75
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-27 15:05:30 +0200

    Adding code samples

---
+++ src/samples/functions.ts
@@ -0,0 +1,7 @@
+function greetings(firstName: string, lastName: string): void {
+    console.log(`Hello ${firstName} ${lastName}`)
+}
+
+function anotherGreetings(firstName: string, lastName: string): string {
+    return `Hello ${firstName} ${lastName}`
+}

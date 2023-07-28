bb5b08536bab4910fff191e68af7d744addf97d1
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-28 11:30:07 +0200

    Continuing adding sample code

--- src/samples/types.ts
+++ src/samples/types.ts
@@ -1,46 +1,60 @@
-function handleUser({
-  firstName,
-  lastName,
-  age,
-  isConnected,
-  isVerified,
-  email
-}: {
+type User = {
+  age: number;
+  books: string[];
+  firstName: string;
+  friends: string[];
+  id: number;
+  lastName: string;
+};
+
+const myUser: User = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 28,
+  books: ["Introduction to Robotics", "Data Structures and Algorithms"],
+  id: 1,
+  friends: ["Jane Smith", "Michael Johnson"],
+};
+
+const anotherUser: {
   firstName: string;
   lastName: string;
   age: number;
-  isConnected: boolean;
-  isVerified: boolean;
-  email: string;
-}) {
-  // Fait quelque chose
+  books: string[];
+  id: number;
+  friends: string[];
+} = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 28,
+  books: ["Introduction to Robotics", "Data Structures and Algorithms"],
+  id: 1,
+  friends: ["Jane Smith", "Michael Johnson"],
+};
+
+
+const myUsers: User[] = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 28,
+    books: ["Introduction to Robotics", "Data Structures and Algorithms"],
+    id: 1,
+    friends: ["Jane Smith", "Michael Johnson"],
+  },
+  {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 30,
+    books: ["Introduction to Robotics", "Data Structures and Algorithms"],
+    id: 2,
+    friends: ["John Doe", "Michael Johnson"],
+  }
+]
+
+function mergeUsers(user: User): User[] {
+  return [
+    user
+  ]
 }
 
-type User = {
-    firstName: string;
-    lastName: string;
-    age: number;
-    isConnected: boolean;
-    isVerified: boolean;
-    email: string;
-}
-
-function anotherHandleUser({
-    firstName,
-    lastName,
-    age,
-    isConnected,
-    isVerified,
-    email
-}: User) {
-    // Fait quelque chose
-}
-
-const me: User = {
-    firstName: 'thomas',
-    lastName: 'dimnet',
-    age: 34,
-    isConnected: true,
-    isVerified: true,
-    email: 'thomas.dimnet@gmail.com'
-}
--- src/samples/variables.ts
+++ src/samples/variables.ts
@@ -1,14 +1,14 @@
 // Je définies explicitement un type aux constantes firstName et age
-const firstName: string = 'Thomas'
-let age: number = 34
+const firstName: string = "Thomas";
+let age: number = 34;
 
 // Je laisse TypeScript s'occuper de la déclaration du type pour moi
-const lastName = 'Dimnet'
-let postCode = 75020
+const lastName = "Dimnet";
+let postCode = 75020;
 
 // Je définis un type plus "complexe"
-const friends: string[] = ['Alex', 'Karl', 'Ashley']
-const friendsAge: number[] = [31, 33, 29]
+const friends: string[] = ["Alex", "Karl", "Ashley"];
+const friendsAge: number[] = [31, 33, 29];
 
 // Je modifie `friends` en lui ajoutant un boolean
-friends.push("Max")
+friends.push("Max");

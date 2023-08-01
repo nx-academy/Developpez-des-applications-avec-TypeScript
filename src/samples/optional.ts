a1d1ea35fbbc1f0802e38923cbe064eb283c5788
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-01 11:11:07 +0200

    Adding code samples for optional

--- src/samples/optional.ts
+++ src/samples/optional.ts
@@ -6,3 +6,18 @@ type MyUser = {
   id: number;
   lastName: string;
 };
+
+const aUser: MyUser = {
+  firstName: "Jane",
+  lastName: "Smith",
+  age: 30,
+  id: 2,
+  friends: ["John Doe", "Michael Johnson"],
+};
+
+function displayBooks(books: string[]): void {
+  console.log(books);
+}
+
+// À decommenter si vous souhaitez voir l'erreur dans VSCode
+// displayBooks(aUser.books)

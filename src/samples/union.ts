86d0ccf67524f432f34f62edb7dd260fe93afa61
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-02 10:42:17 +0200

    Adding another example

---
+++ src/samples/union.ts
@@ -0,0 +1,18 @@
+function displayName(firstName: string, lastName: string): string | Error {
+  if (!firstName || !lastName) {
+    return new Error('firstName or lastName is missing')
+  }
+  return `${firstName} ${lastName}`
+}
+
+
+type AllUser = {
+    age: number;
+    books?: string[];
+    genre: ["he", "his"] | ["she", "her"]
+    firstName: string;
+    friends: string[];
+    id: number;
+    lastName: string;
+}
+
